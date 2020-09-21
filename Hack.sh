bda83dcae85cee714c75ab8fb56c484178296023
anjing85 <70914170+anjing85@users.noreply.github.com>
2020-09-22 00:44:05 +0700

    Hack.sh
    Vvv

---
+++ Hack.sh
@@ -0,0 +1,19 @@
+clear
+
+figlet "Hack Fb"
+
+echo " daftar pilihan "
+echo "1.hack masal "
+echo "2.keluar "
+read -p " pilih Mana : " bro
+
+
+if [ $bro ] [ 1 ] || [ $bro ] [ 1 ]
+then
+clear
+fi
+figlet "Welcome"
+echo " facebook Tools "
+echo
+read -p " masukan id : " bro
+https://github.com/anjing85/hack.sh
